7a796f1b5bc60acf75c112e876209ae46891648d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-04-29 22:52:34 +0900

    Initial Commit

---
+++ DDDaysBackView/DDDaysBackViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDDaysBackViewApp.swift
+//  DDDaysBackView
+//
+//  Created by joe on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DDDaysBackViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
